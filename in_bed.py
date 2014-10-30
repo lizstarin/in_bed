df1ea404b7e99a9269662fefb31d1b5b2f4f5a31
Liz Starin <liz.starin@gmail.com>
2014-10-30 17:20:27 -0400

    Initial commit

---
+++ in_bed.py
@@ -0,0 +1,27 @@
+from twitter import *
+import random, threading, re
+
+t = Twitter(
+    auth=OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
+
+def tweet():
+	status = create_tweet()
+	try:
+		t.statuses.update(status=status)
+	except:
+		print "error"
+	threading.Timer(60, tweet).start()
+
+def create_tweet():
+	statuses = t.search.tweets(q='#fortunecookie "you are" OR "you will" OR "you must"', count=1000)['statuses']
+	status = random.choice(statuses)
+	text = status['text']
+	screen_name = '@' + status['user']['screen_name']
+	
+	parsed_text = ' '.join([word for word in text.split() if word[0] != '#' and word[0] != '@' and not re.match('http:\/\/*', word) and not re.match('^RT$', word)])	
+	return ' '.join([screen_name, parsed_text.strip(',.-;"'), 'in bed.'])
+
+tweet()
+
+
+
